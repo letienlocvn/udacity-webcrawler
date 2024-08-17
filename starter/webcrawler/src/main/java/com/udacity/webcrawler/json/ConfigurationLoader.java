3ff9c9ca7ac62a19f3ecd49f51fbddd0c3db716f
Le Tien Loc <letienlocvn@gmail.com>
2024-08-17 10:53:40 +0700

    Read Json Input from file

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/json/ConfigurationLoader.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/json/ConfigurationLoader.java
@@ -1,8 +1,12 @@
 package com.udacity.webcrawler.json;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
+import java.io.IOException;
 import java.io.Reader;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
 
@@ -11,39 +15,43 @@ import java.util.Objects;
  */
 public final class ConfigurationLoader {
 
-  private final Path path;
+    private final Path path;
 
-  /**
-   * Create a {@link ConfigurationLoader} that loads configuration from the given {@link Path}.
-   */
-  public ConfigurationLoader(Path path) {
-    this.path = Objects.requireNonNull(path);
-  }
+    /**
+     * Create a {@link ConfigurationLoader} that loads configuration from the given {@link Path}.
+     */
+    public ConfigurationLoader(Path path) {
+        this.path = Objects.requireNonNull(path);
+    }
 
-  /**
-   * Loads configuration from this {@link ConfigurationLoader}'s path
-   *
-   * @return the loaded {@link CrawlerConfiguration}.
-   */
-
-
-  public CrawlerConfiguration load() {
-
-    return new CrawlerConfiguration.Builder().build();
-  }
-
-  /**
-   * Loads crawler configuration from the given reader.
-   *
-   * @param reader a Reader pointing to a JSON string that contains crawler configuration.
-   * @return a crawler configuration
-   */
-  public static CrawlerConfiguration read(Reader reader) {
-    // This is here to get rid of the unused variable warning.
-    Objects.requireNonNull(reader);
-    // TODO: Fill in this method
+    /**
+     * Loads configuration from this {@link ConfigurationLoader}'s path
+     *
+     * @return the loaded {@link CrawlerConfiguration}.
+     */
 
 
-    return new CrawlerConfiguration.Builder().build();
-  }
+    public CrawlerConfiguration load() {
+        try (Reader reader = Files.newBufferedReader(path)) {
+            return read(reader);
+        } catch (Exception e) {
+            e.getStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Loads crawler configuration from the given reader.
+     *
+     * @param reader a Reader pointing to a JSON string that contains crawler configuration.
+     * @return a crawler configuration
+     */
+    public static CrawlerConfiguration read(Reader reader) throws IOException {
+        // This is here to get rid of the unused variable warning.
+        Objects.requireNonNull(reader);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(JsonParser.Feature.AUTO_CLOSE_SOURCE);
+
+        return objectMapper.readValue(reader, CrawlerConfiguration.Builder.class).build();
+    }
 }
